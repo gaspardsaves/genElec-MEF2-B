3c6fa222f3012f5e1e2c08e500b34339015cb0ed
gaspardsaves <166817933+gaspardsaves@users.noreply.github.com>
2024-12-02 11:58:30 +0100

    Update script shell arg typePost and help OK

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ Pour compiler les fichiers C seuls : Utiliser la commande 'make -C ./codeC'
 Pour supprimer les exécutables : Utiliser la commande 'make cleanexec -C ./codeC'
 
 Exécution :
-Le fichier de données à traiter doit se situer dans le sous-répertoire 'inputs' (--> à voir pour passer les données en param)
 Vérifier qu'on a les droits d'exécution sur le script shell si non : 'chmod +x c-wire.sh'
-Exécution du script shell : './c-wire.sh'
+Exécution du script shell : './c-wire.sh' + paramétres
+Pour avoir le détail des paramétres possibles du script : possibilité d'utiliser l'option -h ou --help './c-wire.sh -h' ou './c-wire.sh --help'
+Pour exécuter le fichier code C séparément : ./codeC/execdef
+Le fichier de données à traiter doit se situer dans le sous-répertoire 'inputs' (--> à voir pour passer les données en param)
 Suppression des fichiers tampons : 'make clean -C ./codeC'
--- c-wire.sh
+++ c-wire.sh
@@ -1,46 +1,61 @@
 #!/bin/bash
 
-# Primary data processing
+# Parameters analysis
+
+    # Check if the user need help
+    for arg in "$@"; do
+        if [[ "$arg" == "-h" || "$arg" == "--help" ]] ; then
+            echo "Option d'aide détectée"
+            cat "help.txt"
+        fi
+    done
+
     # Name and adress of the input file
-    inputFile="/home/savesgaspa/Documents/GitHub/genElec-MEF2-B/inputs/c-wire_v00.dat"
-    #inputFile="/home/savesgaspa/Documents/GitHub/genElec-MEF2-B/inputs/c-wire_v25.dat"
+    #inputFile="./inputs/c-wire_v00.dat"
+    #inputFile="./inputs/c-wire_v25.dat"
+    inputFile="$1"
 
-    # Output files
-        # Name and address of the lv buffer file
-        outputFileLv="/home/savesgaspa/Documents/GitHub/genElec-MEF2-B/tmp/buff-lv.dat"
-        # Name and address of the HV-B buffer file
-        outputFileHvb="/home/savesgaspa/Documents/GitHub/genElec-MEF2-B/tmp/buff-hvb.dat"
-         # Name and address of the HV-A buffer file
-        outputFileHva="/home/savesgaspa/Documents/GitHub/genElec-MEF2-B/tmp/buff-hva.dat"
-        
-        # Create or clean the LV buffer file 
-        > "$outputFileLv"
-        # Create or clean the HV-B buffer file 
-        > "$outputFileHvb"
-        # Create or clean the HV-A buffer file 
-        > "$outputFileHva"
-
-    # Data processing
-        # Read every line (except the first (categories)) of the input file
-        tail -n +2 "$inputFile" | while IFS=';' read -r col1 col2 col3 col4 col5 col6 col7 col8; do
+    typeConso="$2"
+    case "$typeConso" in
+        'lv' )
+            # LV data
+            # Name and address of the lv buffer file
+                outputFileLv="./tmp/buff-lv.dat"
+                # Create or clean the LV buffer file 
+                > "$outputFileLv"
+            # Read every line (except the first (categories)) of the input file
             # Check if it's a LV (column 4 and 7 not empty (different of '-'))
-            if [[ "$col4" != "-" && "$col7" != "-" ]]; then
-                    # Add columns in the lv buffer file
-                    echo "$col4;$col7" >> "$outputFileLv"
-            fi
-            # Check if it's a HV-A (column 3 and 7 not empty (different of '-') and column 4 empty)
-            if [[ "$col3" != "-" && "$col4" = "-" && "$col7" != "-" ]]; then
-                    # Add columns in the hv-a buffer file
-                    echo "$col3;$col7" >> "$outputFileHva"
-            fi
-            # Check if it's a HV-B (column 2 and 7 not empty (different of '-') and column 3 empty)
-            if [[ "$col2" != "-" && "$col3" = "-" && "$col7" != "-" ]]; then
-                    # Add columns in the HV-B buffer file
-                    echo "$col2;$col7" >> "$outputFileHvb"
-            fi
-        done
-
-        # Success
+            # Add columns 1, 4 and 7 in the lv buffer file
+            grep -E "^[^-]+;-;[^-]+;[^-]+;-;-;[^-]+;-$" "$inputFile" | cut -d ";" -f1,3,4,7 >> "$outputFileLv"
+            # Success
             echo "Extraction terminée. Les données LV sont dans $outputFileLv"
+        ;;
+        'hva' )
+            # HV-A data
+            # Output file
+                # Name and address of the HV-A buffer file
+                outputFileHva="./tmp/buff-hva.dat"
+                # Create or clean the HV-A buffer file 
+                > "$outputFileHva"
+            # Read every line (except the first (categories)) of the input file
+            # Check if it's a HV-A (column 3 and 7 not empty (different of '-') and column 4 empty)
+            # Add columns 1, 2, 3 and 7 in the HV-A buffer file
+            grep -E "^[^-]+;[^-]+;[^-]+;-;-;-;[^-]+;-$" "$inputFile" | cut -d ";" -f1,2,3,7 >> "$outputFileHva"
+            # Success
             echo "Extraction terminée. Les données HV-A sont dans $outputFileHva"
+        ;;
+        'hvb' )
+            # HV-B data
+            # Output file
+                # Name and address of the HV-B buffer file
+                outputFileHvb="./tmp/buff-hvb.dat"
+                # Create or clean the HV-B buffer file 
+                > "$outputFileHvb"
+            # Read every line (except the first (categories)) of the input file
+            # Check if it's a HV-B (column 2 and 7 not empty (different of '-') and column 3 empty)
+            # Add columns 1, 2 and 7 in the HV-B buffer file
+            grep -E "^[^-]+;[^-]+;-;-;-;-;[^-]+;-$" "$inputFile" | cut -d ";" -f1,2,7 >> "$outputFileHvb"
+            # Success
             echo "Extraction terminée. Les données HV-B sont dans $outputFileHvb"
+        ;;
+    esac
