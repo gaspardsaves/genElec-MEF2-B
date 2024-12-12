7ed3abccd251650de4616e90a574a7e68dedffd0
IBBC78 <99761517+IBBC78@users.noreply.github.com>
2024-12-12 17:20:47 +0000

    gnuplot try update

--- lv_gnuplot.sh
+++ lv_gnuplot.sh
@@ -1,13 +1,14 @@
 set terminal pngcairo size 800,600
 set output "bar.png"
 
-set datafile separator ","
-set style data histograms
-set style fill solid 1.00 border -1
-set xlabel "lv"
-set ylabel "value"
-set title "baton"
-set grid
+set boxwidth 0.5
+set style fill solid 0.5 border -1
+set yrange [0:*]
+set xlabel "Categorie"
+set ylabel "Valeur"
+set title "Graph baton"
 
-plot "f_x.csv" using 2:xtic(1) every ::0::1 title "value" lc rgb "red", \
- "f_x.csv" using 2:xtic(1) every ::2::3 title "value" lc rgb "green"
+set style line 1 lc rgb '#FF0000'
+set style line 2 lc rgb '#00FF00'
+
+plot "test.csv" using  0:2:(column(2) > 25 ? 1 : 2) with boxes linecolor variable title "graph col"
