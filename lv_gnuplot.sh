b22a75921824281bb0ff50db7d33fea58180b33c
IBBC78 <99761517+IBBC78@users.noreply.github.com>
2024-12-14 18:05:11 +0100

    gnuplot update

--- lv_gnuplot.sh
+++ lv_gnuplot.sh
@@ -1,14 +1,21 @@
 set terminal pngcairo size 800,600
 set output "bar.png"
-
 set boxwidth 0.5
 set style fill solid 0.5 border -1
-set yrange [0:*]
-set xlabel "Categorie"
-set ylabel "Valeur"
-set title "Graph baton"
-
-set style line 1 lc rgb '#FF0000'
-set style line 2 lc rgb '#00FF00'
-
-plot "test.csv" using  0:2:(column(2) > 0 ? 1 : 2) with boxes linecolor variable title "graph col"
+set ylabel "ratio"
+set xlabel "ID"
+set yrange [0:*] 
+set xtic font 'arial,12'
+set ytic font 'arial,10'
+set title "Graphique en bâtons"
+set grid
+set boxwidth 1
+set style data histograms
+set style histogram cluster gap 1
+set style fill solid noborder
+set style line 1 lc rgb 'red'
+set style line 2 lc rgb 'green' 
+set datafile separator ";"  
+set key autotitle columnhead  
+set xtics rotate by -45 
+plot "test.csv" using 1:($2 > 0 ? abs($2) : abs($2)):(column(2) > 0 ? 1 : 2) with boxes lc variable notitle
