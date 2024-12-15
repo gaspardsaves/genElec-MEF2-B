789b8f79c09d5ce1276373ba21f7f47c37ffe96c
JEAN-LUC7 <jeanluc.maslanka20@gmail.com>
2024-12-15 17:39:54 +0100

    Add color.h to lv_all.c

--- codeC/lv_all.c
+++ codeC/lv_all.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "color.h"
 
 long Ratio(long capacity, long consumption){
     return capacity-consumption;
@@ -8,6 +9,12 @@ int main(){
     int id;
     long capacity, consumption;
     while (scanf("%d:%ld:%ld", &id, &capacity, &consumption) == 3){
+        if(Ratio(capacity, consumption)<0){
+            color("32");
+        }
+        if(Ratio(capacity, consumption)>=0){
+            color("33");
+        }
         printf("%d:%ld:%ld:%ld\n", id, capacity, consumption, Ratio(capacity, consumption));
     }
     return 0;
